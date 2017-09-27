5bdee481584dbcf95c59102e6c3eb0a2fb6e7045
Monique Jones <monique.jones25@gmail.com>
2017-09-27 01:19:10 +0000

    set up html board and css, set up init board function

---
+++ src/js/initBoard.js
@@ -0,0 +1,11 @@
+// initialize our chess board
+function initBoard () {
+    console.log('initBoard');
+    
+    for(let i = 0; i < 8; i++) {
+        const row = document.createElement('div');
+    }
+    
+}
+
+export default initBoard;
